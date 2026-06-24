6031f0b972923283599b398c20fcc0861d8d01e3
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-06-24 14:15:51 +0200

    feat!: add tts respons

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from vosk import Model, KaldiRecognizer, SetLogLevel
 import yaml
 from rapidfuzz import fuzz
 import random
+from piper import PiperVoice
+import queue
+import threading
+import subprocess
+import tempfile
+import os
+import wave
+
 
 SetLogLevel(-1)
 
+VOICE_PATH = "model/tts/glados/fr_FR-glados-medium.onnx"
 MODEL_PATH = "model/vosk/vosk-model-small-fr/vosk-model-small-fr-0.22"
 SAMPLE_RATE = 16000
 BLOCK_SIZE = 4000
@@ -34,14 +43,29 @@ def check_respons(dico, text):
         return random.choice(best_entry["respons"])
     return None
 
+def player_worker(q: "queue.Queue[str | None]"):
+    while True:
+        path = q.get()
+        if path is None:
+            return
+        subprocess.run(["mpv", "--no-terminal", "--really-quiet", path])
+        try:
+            os.remove(path)
+        except OSError:
+            pass
+
 def main():
     model = Model(MODEL_PATH)
     rec = KaldiRecognizer(model, SAMPLE_RATE)
+    on_use = False
 
     with open("test.yaml", "r") as file:
         dico = yaml.safe_load(file)
     print(dico)
     print("\n")
+
+    # init phase for tts (for test)
+    voice = PiperVoice.load(VOICE_PATH)
 
     print("En écoute... (Ctrl+C pour arrêter)")
 
@@ -54,7 +78,12 @@ def main():
     ):
         while True:
             data = audio_queue.get()
-            if rec.AcceptWaveform(data):
+            audio_q: "queue.Queue[str | None]" = queue.Queue()
+            t = threading.Thread(target=player_worker, args=(audio_q,), daemon=True)
+            t.start()
+            
+            if rec.AcceptWaveform(data) and on_use == False:
+                on_use = True
                 result = json.loads(rec.Result())
                 text = result.get("text", "").strip()
                 if text:
@@ -63,9 +92,20 @@ def main():
                     text_lower = text.lower()
                     respons = check_respons(dico, text_lower)
                     print(respons)
+                    if respons:
+                        fd, path = tempfile.mkstemp(prefix="tts_", suffix=".wav")
+                        os.close(fd)
+                        with wave.open(path, "wb") as wav_file:
+                            voice.synthesize_wav(respons, wav_file)
+                        audio_q.put(path)
+
+
             else:
                 partial = json.loads(rec.PartialResult())
                 p = partial.get("partial", "").strip()
+            audio_q.put(None)
+            t.join()
+            on_use = False
 
 if __name__ == "__main__":
     try:
